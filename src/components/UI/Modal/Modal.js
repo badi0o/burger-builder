2844db1c1902ef020c9b7e3aad1df6a2fbab0f17
badi0o <46631416+badi0o@users.noreply.github.com>
2019-12-08 23:03:29 +0300

    lots of changes yet again
    error handling added, spinner added to when theres communication between the server and the website,
    ingredients are now on the server and not local anymore, and many more changes

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,29 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./Modal.module.css";
 import Aux from "../../../hoc/Auxiliary/Auxiliary";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = props => {
-  return (
-    <Aux>
-      <Backdrop click={props.modalClosed} show={props.show} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0"
-        }}
-      >
-        {props.children}
-      </div>
-    </Aux>
-  );
-};
-const areEqual = (prevProps, nextProps) => {
-  if (prevProps.show === nextProps.show) {
-    return true;
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop click={this.props.modalClosed} show={this.props.show} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
   }
 };
 
-export default React.memo(modal, areEqual);
+export default Modal;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import Aux from "../../hoc/Auxiliary/Auxiliary";
 import Modal from "../../components/UI/Modal/Modal";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -15,16 +17,22 @@ const INGREDIENT_PRICES = {
 
 class BurgerBuilder extends Component {
   state = {
-    ingredients: {
-      salad: 0,
-      bacon: 0,
-      cheese: 0,
-      meat: 0
-    },
+    ingredients: null,
     totalPrice: 4,
     purchasable: false,
-    purchasing: false
+    purchasing: false,
+    loading: false,
+    error: false
   };
+  componentDidMount() {
+    axios.get('https://react-my-burger-bado.firebaseio.com/ingredients.json')
+      .then(res => {
+        this.setState({ ingredients: res.data });
+      })
+      .catch(error => {
+        this.setState({ error: true });
+      });
+  }
 
   updatePuchaseState(ingredients) {
     const sum = Object.keys(ingredients)
@@ -76,14 +84,71 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("you continue");
+
+    // alert("you continue");
+
+    this.setState({ loading: true })
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice.toFixed(2),
+      customer: {
+        name: 'Helo Belo',
+        address: {
+          street: 'teststreet',
+          zipCode: '430290',
+          country: 'Kurdistan'
+        },
+        email: 'test@tes.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+    axios.post('/orders.json', order)
+      .then(response => {
+        console.log(response)
+        this.setState({ loading: false, purchasing: false })
+      })
+      .catch(error => {
+        this.setState({ loading: false, purchasing: false })
+      });
   };
   render() {
     const disableInfo = {
       ...this.state.ingredients
     };
+
     for (let key in disableInfo) {
       disableInfo[key] = disableInfo[key] <= 0;
+    }
+
+    let orderSummary = null;
+
+    let burger = this.state.error ? <p>Ingredients cant be loaded</p> : <Spinner />
+
+    if (this.state.ingredients) {
+      burger = (
+        <Aux>
+          <Burger ingredients={this.state.ingredients} />
+          <BuildControls
+            igadd={this.addIngredientHandler}
+            igremove={this.removeIngredientHandler}
+            disabled={disableInfo}
+            purchasable={this.state.purchasable}
+            price={this.state.totalPrice}
+            ordered={this.purchaseHandler}
+          />
+        </Aux>
+      );
+      orderSummary = <OrderSummary
+        price={this.state.totalPrice.toFixed(2)}
+        ingredients={this.state.ingredients}
+        purchaseCancelled={this.purchaseHandlerNegate}
+        purchaseContinue={this.purchaseContinueHandler}
+
+      />;
+    }
+
+    if (this.state.loading) {
+      orderSummary = <Spinner />
     }
 
     return (
@@ -92,25 +157,12 @@ class BurgerBuilder extends Component {
           modalClosed={this.purchaseHandlerNegate}
           show={this.state.purchasing}
         >
-          <OrderSummary
-            price={this.state.totalPrice.toFixed(2)}
-            ingredients={this.state.ingredients}
-            purchaseCancelled={this.purchaseHandlerNegate}
-            purchaseContinue={this.purchaseContinueHandler}
-          />
+          {orderSummary}
         </Modal>
-        <Burger ingredients={this.state.ingredients} />
-        <BuildControls
-          igadd={this.addIngredientHandler}
-          igremove={this.removeIngredientHandler}
-          disabled={disableInfo}
-          purchasable={this.state.purchasable}
-          price={this.state.totalPrice}
-          ordered={this.purchaseHandler}
-        />
+        {burger}
       </Aux>
     );
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
